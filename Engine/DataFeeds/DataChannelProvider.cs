38c667bc3e3fff1137fd4c81392d27c15bc442b4
Stefano Raggi <stefano.raggi67@gmail.com>
2020-04-07 23:31:26 +0200

    Test enable streaming all custom types

--- Engine/DataFeeds/DataChannelProvider.cs
+++ Engine/DataFeeds/DataChannelProvider.cs
@@ -15,7 +15,13 @@
 
 using System;
 using QuantConnect.Data;
+using QuantConnect.Data.Custom.Benzinga;
 using QuantConnect.Packets;
+using QuantConnect.Data.Custom.Estimize;
+using QuantConnect.Data.Custom.PsychSignal;
+using QuantConnect.Data.Custom.SmartInsider;
+using QuantConnect.Data.Custom.Tiingo;
+using QuantConnect.Data.Custom.TradingEconomics;
 
 namespace QuantConnect.Lean.Engine.DataFeeds
 {
@@ -29,6 +35,19 @@ namespace QuantConnect.Lean.Engine.DataFeeds
         /// </summary>
         public virtual bool ShouldStreamSubscription(LiveNodePacket job, SubscriptionDataConfig config)
         {
+            if (config.Type == typeof(TiingoNews) ||
+                config.Type == typeof(PsychSignalSentiment) ||
+                config.Type == typeof(EstimizeRelease) ||
+                config.Type == typeof(EstimizeConsensus) ||
+                config.Type == typeof(EstimizeEstimate) ||
+                config.Type == typeof(TradingEconomicsCalendar) ||
+                config.Type == typeof(BenzingaNews) ||
+                config.Type == typeof(SmartInsiderIntention) ||
+                config.Type == typeof(SmartInsiderTransaction))
+            {
+                return true;
+            }
+
             return IsStreamingType(config) || !config.IsCustomData;
         }
 
