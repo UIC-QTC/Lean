3c6dd4ec31d989b2fd669fd9b19d503074d37e83
Stefano Raggi <stefano.raggi67@gmail.com>
2020-07-30 14:44:56 +0200

    Remove IDataQueueHandler from AlpacaBrokerage
    - a new IDataQueueHandler implementation for Polygon.io will be added to the ToolBox

--- Tests/Brokerages/Alpaca/AlpacaBrokerageHistoryProviderTests.cs
+++ Tests/Brokerages/Alpaca/AlpacaBrokerageHistoryProviderTests.cs
@@ -67,7 +67,7 @@ namespace QuantConnect.Tests.Brokerages.Alpaca
             var secretKey = Config.Get("alpaca-secret-key");
             var tradingMode = Config.Get("alpaca-trading-mode");
 
-            using (var brokerage = new AlpacaBrokerage(null, null, keyId, secretKey, tradingMode, true))
+            using (var brokerage = new AlpacaBrokerage(null, null, keyId, secretKey, tradingMode))
             {
                 var historyProvider = new BrokerageHistoryProvider();
                 historyProvider.SetBrokerage(brokerage);
